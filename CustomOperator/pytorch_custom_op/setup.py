06eaef695a46e94a7bba572cde5c8fa854365f77
Negin Mohammadi Raoof <neraoof@microsoft.com>
2019-11-19 18:59:58 -0800

    clean up references and files

--- CustomOperator/pytorch_custom_op/setup.py
+++ CustomOperator/pytorch_custom_op/setup.py
@@ -3,5 +3,5 @@ from torch.utils import cpp_extension
 
 setup(name='custom_group_norm',
       ext_modules=[cpp_extension.CppExtension('custom_group_norm', ['custom_group_norm.cpp'],
-                                              include_dirs = ['/home/neraoof/onnxruntime/cmake/external/eigen'])],
+                                              include_dirs = [<PATH_TO_EIGEN_HEADER>])],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
