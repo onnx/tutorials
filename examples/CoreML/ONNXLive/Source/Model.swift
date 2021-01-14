64937a6561cc94b2486529ea16117462d15ac0a7
Sheng Zha <zhasheng@amazon.com>
2021-01-13 19:39:25 -0500

    add ALv2 licenses
    Signed-off-by: Sheng Zha <zhasheng@amazon.com>

--- examples/CoreML/ONNXLive/Source/Model.swift
+++ examples/CoreML/ONNXLive/Source/Model.swift
@@ -1,8 +1,5 @@
 /**
- * Copyright (c) Facebook, Inc. and Microsoft Corporation.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 import Foundation
--- tutorials/output/README.md
+++ tutorials/output/README.md
@@ -1,1 +1,3 @@
+<!--- SPDX-License-Identifier: Apache-2.0 -->
+
 ipython notebooks from enclosing directory generate their outputs here. Folder is created for convenience, we don't check in those files
